7b1bd54f8153f8969fc6edad889fcd8486850122
ryuseok0000 <163639171+ryuseok0000@users.noreply.github.com>
2024-03-16 18:16:00 +0900

    20240316

---
+++ 1-2.py
@@ -0,0 +1,34 @@
+print('사과',2,'바나나',3 )
+print('사과', '바나나', sep="!!!")
+print('사과','바나나', end='')
+print('아보카도')
+print('{}와 {}'.format('사과','바나나'))
+print('{1}와 {0}'.format('사과','바나나'))
+a='사과'
+b='바나나'
+print(f'{a}와{b}')
+print('사과\n바나나')
+print('''사과
+바나나''')
+print('사과\t바나나')
+
+print("we learn python!"[:2])
+print(53==35)
+print('류현석'*5)
+print(2<9 and 9==9)
+print(0<=3 and 72<=72)
+print(12<19 or 1<9)
+print(4==4 or 2<12)
+print( not 12<19)
+print(not 4!=4)
+print('당신의 이름을 입력하십시오')
+print('류현석')
+print('류현석,류,18')
+print('사과', '바나나', '아보카도', sep='!!!')
+print('사과', end='!!!')
+print('바나나', end='!!!')
+print('아보카도', end='!!!')
+name1 = 2022
+name2 = '메가스터디'
+name3 = 'SW입시센터'
+name1+name2+name3
